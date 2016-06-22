3b8c9384533c2583d7c68af4a82c9277ba2878a5
Sean Stephens <seanstephens29@gmail.com>
2016-06-22 14:20:37 -0600

    added bluprints

---
+++ blueprints/ember-paper-time-picker/index.js
@@ -0,0 +1,17 @@
+/* jshint node: true */
+'use strict';
+
+module.exports = {
+  normalizeEntityName: function() {},
+
+  afterInstall: function() {
+	var _this = this;
+	return this.addAddonToProject('ember-pikaday@1.1.0').then(function()
+	{
+		return _this.addAddonToProject('ember-snap-svg@0.0.6').then(function()
+		{
+			return _this.addAddonToProject('ember-moment@6.1.0');
+		});
+	});
+  }
+};
