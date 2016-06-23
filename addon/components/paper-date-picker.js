3c7713c0dd678fe92246672da058d6c87d7d6167
Jacob Pedersen <jacob.pedersen@busybusy.com>
2016-06-23 16:55:05 -0600

    header for date picker

--- addon/components/paper-date-picker.js
+++ addon/components/paper-date-picker.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import moment from 'moment';
 import layout from '../templates/components/paper-date-picker';
 
 export default Ember.Component.extend({
@@ -8,5 +9,125 @@ export default Ember.Component.extend({
     timestamp: null,
     minDate: null,
     maxDate: null,
-    format: null,
+
+    day: null,
+    month: null,
+    year: null,
+
+    init: function()
+    {
+        this._super();
+
+    },
+
+    initDayOfWeek: Ember.on('init', Ember.observer('timestamp', function() {
+        let time = this.get('timestamp');
+        let momentObj = moment(time);
+        let newFormat = momentObj.format('dddd');
+
+        this.set('dayOfWeek', newFormat);
+    })),
+
+    observesDayOfWeek: Ember.observer('timestamp', function()
+    {
+        let time = this.get('timestamp');
+        let momentObj = moment(time);
+        let newFormat = momentObj.format('dddd');
+
+        this.set('dayOfWeek', newFormat);
+    }),
+
+    initDay: Ember.on('init', Ember.observer('timestamp', function() {
+        let time = this.get('timestamp');
+        let momentObj = moment(time);
+        let newFormat = momentObj.format('DD');
+
+        this.set('day', newFormat);
+    })),
+
+    observesDay: Ember.observer('timestamp', function()
+    {
+        let time = this.get('timestamp');
+        let momentObj = moment(time);
+        let newFormat = momentObj.format('DD');
+
+        this.set('day', newFormat);
+    }),
+
+    initMonth: Ember.on('init', Ember.observer('timestamp', function() {
+        let time = this.get('timestamp');
+        let momentObj = moment(time);
+        let newFormat = (momentObj.format('MMM')).toUpperCase();
+
+        this.set('month', newFormat);
+    })),
+
+    observesMonth: Ember.observer('timestamp', function()
+    {
+        let time = this.get('timestamp');
+        let momentObj = moment(time);
+        let newFormat = (momentObj.format('MMM')).toUpperCase();
+
+        this.set('month', newFormat);
+    }),
+
+    initYear: Ember.on('init', Ember.observer('timestamp', function() {
+        let time = this.get('timestamp');
+        let momentObj = moment(time);
+        let newFormat = momentObj.format('YYYY');
+
+        this.set('year', newFormat);
+    })),
+
+    observesYear: Ember.observer('timestamp', function()
+    {
+        let time = this.get('timestamp');
+        let momentObj = moment(time);
+        let newFormat = momentObj.format('YYYY');
+
+        this.set('year', newFormat);
+    }),
+
+    actions: {
+
+        subtractMonth()
+        {
+            let timestamp = this.get('timestamp');
+            let object = moment(timestamp);
+            let subtract = object.subtract('1', 'months');
+            let reverse = subtract.unix() * 1000;
+
+            this.set('timestamp', reverse);
+        },
+
+        addMonth()
+        {
+            let timestamp = this.get('timestamp');
+            let object = moment(timestamp);
+            let add = object.add('1', 'months');
+            let reverse = add.unix() * 1000;
+
+            this.set('timestamp', reverse);
+        },
+
+        subtractYear()
+        {
+            let timestamp = this.get('timestamp');
+            let object = moment(timestamp);
+            let subtract = object.subtract('1', 'years');
+            let reverse = subtract.unix() * 1000;
+
+            this.set('timestamp', reverse);
+        },
+
+        addYear()
+        {
+            let timestamp = this.get('timestamp');
+            let object = moment(timestamp);
+            let add = object.add('1', 'years');
+            let reverse = add.unix() * 1000;
+
+            this.set('timestamp', reverse);
+        },
+    }
 });
