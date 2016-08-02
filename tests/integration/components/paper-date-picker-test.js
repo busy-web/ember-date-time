08ba495587891def9530a997920db46d95c77798
Jacob Pedersen <jacob.pedersen@busybusy.com>
2016-08-02 13:26:26 -0600

    test(component): add render tests

--- tests/integration/components/paper-date-picker-test.js
+++ tests/integration/components/paper-date-picker-test.js
@@ -1,14 +1,16 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
 
 moduleForComponent('paper-date-picker', 'Integration | Component | paper date picker', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-  this.render(hbs`{{paper-date-picker}}`);
 
-  console.log(this.$('.valid-day').text());
+  this.set('timestamp', moment());
+
+  this.render(hbs`{{paper-date-picker timestamp=timestamp}}`);
+
+  assert.ok(this.$().text().trim());
 });
--- tests/integration/components/paper-time-picker-test.js
+++ tests/integration/components/paper-time-picker-test.js
@@ -1,15 +1,25 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
 
 moduleForComponent('paper-time-picker', 'Integration | Component | paper time picker', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{complete-time-picker}}`);
+  this.set('timestamp', moment());
+
+  this.render(hbs`{{paper-time-picker timestamp=timestamp}}`);
+
+  assert.ok(this.$().text().trim());
+});
+
+test('changes from hours to minutes', function(assert) {
+
+  this.set('timestamp', moment());
+
+  this.render(hbs`{{paper-time-picker timestamp=timestamp}}`);
 
   assert.equal(this.$('.hours-header').hasClass('active'), true);
   assert.equal(this.$('.minutes-header').hasClass('inactive'), true);
