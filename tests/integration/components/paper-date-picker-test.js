e847af2721e8639437d0ba27d96459c7eef6b471
Jacob Pedersen <jacob.pedersen@busybusy.com>
2016-08-05 14:06:00 -0600

    tests(component): add tests for paper-date-picker

--- tests/integration/components/paper-date-picker-test.js
+++ tests/integration/components/paper-date-picker-test.js
@@ -14,3 +14,59 @@ test('it renders', function(assert) {
 
   assert.ok(this.$().text().trim());
 });
+
+test('check calender values', function(assert) {
+
+  this.set('timestamp', moment());
+
+  this.render(hbs`{{paper-date-picker timestamp=timestamp}}`);
+
+  assert.equal(this.$('.day-of-week').text().trim(), this.get('timestamp').format('dddd'));
+
+  assert.equal(this.$('.month').text().trim(), this.get('timestamp').format('MMM').toUpperCase());
+
+  assert.equal(this.$('.day-of-month').text().trim(), this.get('timestamp').format('DD'));
+
+  assert.equal(this.$('.year').text().trim(), this.get('timestamp').format('YYYY'));
+
+  assert.equal(this.$('.month-year').text().trim(), this.get('timestamp').format('MMMM YYYY'));
+});
+
+test('add and subtract months from calender', function(assert) {
+
+  this.set('timestamp', moment());
+  this.set('calenderTimestampAdd', moment());
+  this.set('calenderTimestampSubtract', moment());
+
+  let addMonth = this.get('calenderTimestampAdd').add('1', 'months').format('MMMM YYYY');
+  let subtractMonth = this.get('calenderTimestampSubtract').subtract('1', 'months').format('MMMM YYYY');
+
+  this.render(hbs`{{paper-date-picker timestamp=timestamp}}`);
+
+  this.$('.add-month').click();
+  assert.equal(this.$('.month-year').text().trim(), addMonth);
+
+  this.$('.subtract-month').click();
+  this.$('.subtract-month').click();
+
+  assert.equal(this.$('.month-year').text().trim(), subtractMonth);
+});
+
+test('click on day', function(assert) {
+
+  this.set('timestamp', moment().startOf('month'));
+  this.set('nextDay', moment().startOf('month'));
+
+  let firstDay = this.get('timestamp').date();
+  let nextDay = this.get('nextDay').add('1', 'days').date();
+
+  this.render(hbs`{{paper-date-picker timestamp=timestamp}}`);
+
+  this.$('.valid-day:first').next().click();
+
+  assert.equal(this.$('.valid-day:first').next().html(), nextDay);
+
+  this.$('.valid-day:first').click();
+
+  assert.equal(this.$('.valid-day:first').html(), firstDay);
+});
