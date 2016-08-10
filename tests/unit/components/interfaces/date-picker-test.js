10a01eb3c069b363178bda891a35af8eade1ca44
Jacob Pedersen <jacob.pedersen@busybusy.com>
2016-08-10 14:06:48 -0600

    refactor/test(component): refactor and add unit tests for date-picker

--- tests/unit/components/interfaces/date-picker-test.js
+++ tests/unit/components/interfaces/date-picker-test.js
@@ -1,14 +1,43 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import moment from 'moment';
 
 moduleForComponent('interfaces/date-picker', 'Unit | Component | interfaces/date picker', {
+  needs: ['helper:format-day'],
   unit: true
 });
 
 test('it renders', function(assert) {
 
-  // Creates the component instance
-  /*let component =*/ this.subject();
-  // Renders the component to the page
+  const time = moment().unix() * 1000;
+  const args = {'timestamp': time};
+
+  this.subject(args);
   this.render();
-  assert.equal(this.$().text().trim(), '');
+  assert.ok(this.$().text().trim());
+});
+
+test('set timestamp', function(assert) {
+
+  const time = moment().unix() * 1000;
+  const args = {'timestamp': time};
+
+  let component = this.subject(args);
+
+  let newTimestamp = moment().add('1', 'days');
+  component.setTimestamp(newTimestamp);
+
+  assert.equal(component.get('timestamp'), newTimestamp.unix() * 1000);
+});
+
+test('set calender date', function(assert) {
+
+  const time = moment().unix() * 1000;
+  const args = {'timestamp': time};
+
+  let component = this.subject(args);
+
+  let newTimestamp = moment().add('6', 'days');
+  component.setCalenderDate(newTimestamp);
+
+  assert.equal(component.get('calenderDate'), newTimestamp.unix() * 1000);
 });
