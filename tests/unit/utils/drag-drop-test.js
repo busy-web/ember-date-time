583ba92212cc9680ce10e0bd0a4fdc8e7cb1670c
Jacob Pedersen <jacob.pedersen@busybusy.com>
2016-09-01 16:36:07 -0600

    add files

---
+++ tests/unit/utils/drag-drop-test.js
@@ -0,0 +1,10 @@
+import DragDrop from 'dummy/utils/drag-drop';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | drag drop');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = DragDrop.create();
+  assert.ok(result);
+});
