46a5b701537acaca2c85bc34b4a9c39798530d10
Sean Stephens <seanstephens29@gmail.com>
2016-11-02 14:51:56 -0600

    fixed install process

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 /* jshint node: true */
 'use strict';
 
-//var Funnel = require('broccoli-funnel');
-//var mergeTrees = require('broccoli-merge-trees');
-//var path = require('path');
-
 module.exports = {
   name: 'ember-paper-time-picker',
 
@@ -17,39 +13,5 @@ module.exports = {
 		}
 
 		this.app = app;
-//		this.importBrowserDeps(app);
-  },
-
-//  importBrowserDeps(app) {
-//    if(app.import) {
-//			var vendor = this.treePaths.vendor;
-//			//app.import(vendor + '/crypto-js/crypto-js.js', {prepend: true});
-//			//app.import(vendor + '/node-uuid/uuid.js', {prepend: true});
-//			app.import('vendor/shims.js');
-//		}
-//	},
-//
-//	treeForVendor(vendorTree) {
-//		var trees = [];
-//
-//		if (vendorTree) {
-//			trees.push(vendorTree);
-//		}
-//
-//	//	var cryptoPath = path.dirname(require.resolve('crypto-js'));
-//
-//	//	trees.push(new Funnel(cryptoPath, {
-//	//		destDir: 'crypto-js',
-//	//		include: [new RegExp(/\.js$/)]
-//	//	}));
-//
-//	//	var nodeUUID = path.dirname(require.resolve('node-uuid'));
-//
-//	//	trees.push(new Funnel(nodeUUID, {
-//	//		destDir: 'node-uuid',
-//	//		include: [new RegExp(/\.js$/)]
-//	//	}));
-//
-//		return mergeTrees(trees);
-//	}
+  }
 };
