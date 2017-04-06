c9e9bc3ca9d2bc68193e15992004beec06c45f65
Sean Stephens <seanstephens29@gmail.com>
2017-04-06 17:50:36 -0600

    fix(packages): updated npm packages

--- blueprints/ember-paper-time-picker/index.js
+++ blueprints/ember-paper-time-picker/index.js
@@ -5,6 +5,8 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addAddonsToProject({packages: ['ember-snap-svg@0.0.6', 'busy-utils@2.3.6']});
+    return this.addAddonToProject("ember-snap-svg", "0.0.6").then(() => {
+			return this.addAddonToProject("busy-utils", "2.4.0");
+		});
   }
 };
