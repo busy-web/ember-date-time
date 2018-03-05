59a6804f1aa1fefe9f3b0b00b850ccf5c56815c8
Sean Stephens <seanstephens29@gmail.com>
2018-03-05 12:33:00 -0700

    fix(util): format parser not parsing ", " correctly

--- addon/utils/format.js
+++ addon/utils/format.js
@@ -5,8 +5,17 @@
 import { isEmpty } from '@ember/utils';
 import moment from 'moment';
 
-const REGX = new RegExp(/\/|\.|-|,| |:/);
+// regular expression to split date format into format sections
+const REGX = new RegExp(/[/.\-, :]+/);
 
+/**
+ * Returns the long format for localized formats like `ll`
+ *
+ * @public
+ * @method longFormatDate
+ * @param format {string} moment format string
+ * @return {string} moment format string
+ */
 export function longFormatDate(format) {
 	const localeData = moment.localeData();
 	let parts = format.split(' ');
@@ -26,24 +35,56 @@ export function longFormatDate(format) {
 	return format;
 }
 
+/**
+ * split string into smaller moment format types like `dd` and `mm`
+ *
+ * @public
+ * @method splitFormat
+ * @param str {string} moment format string
+ * @return {string[]} moment format parts
+ */
 export function splitFormat(str) {
 	return str.split(REGX);
 }
 
+/**
+ * Create a map of each char to the format sections for ui clicks.
+ * This allows the input to place a cursor on a section of the date.
+ *
+ * @private
+ * @method createSectionMap
+ * @param str {string} moment date format string
+ * @return {object}  map[Map] and sections[] are returned in the object.
+ */
 function createSectionMap(str) {
 	let split = splitFormat(str);
 	let map = new window.Map();
 	let sections = [];
-	let start = 0, end = 0, index = 0;
+	let start = 0,		// index for the start of a section
+			end = 0,			// index for the end of a section
+			index = 0,		// tracks the current section for indexing each char
+			parsed = 0;		// prevents section changes due to multiple splitter types in a row like `, `
+
+	// loop over each char and assign it to the a section
+	// for example:
+	//    MM/DD/YYYY
+	//    0001112222
+	//
+	// when the user clicks on the first `/` char the map will place the cursor
+	// in the `0` indexed section or `MM` section type
 	for(let i=0; i<str.length; i++) {
 		end = start + split[index].length;
 		map.set(i, index);
-		if (REGX.test(str[i])) {
+		if (REGX.test(str[i]) && parsed > 0) {
 			sections.push({ start, end });
+			parsed = 0;
 			index += 1;
 			start = i+1;
+		} else {
+			parsed += 1;
 		}
 	}
+
 	sections.push({ start, end });
 	return { map, sections };
 }
@@ -56,9 +97,9 @@ function createSectionMap(str) {
  *
  * @private
  * @method normalizeIndex
- * @params key {number} the value to force within the bounds
- * @params startBound {number} min value ie. 0
- * @params endBound {number} max value ie. Array.length
+ * @param key {number} the value to force within the bounds
+ * @param startBound {number} min value ie. 0
+ * @param endBound {number} max value ie. Array.length
  * @return {number}
  */
 function normalizeIndex(key, startBound, endBound) {
@@ -71,12 +112,11 @@ function normalizeIndex(key, startBound, endBound) {
 }
 
 export function getCursorPosition(format, value, index, isAdd=false, isSub=false) {
-	//console.log('format', value, index);
 	index = normalizeIndex(index, 0, value.length - 1);
 	let { vmap } = findSectionIndex(format, value);
 	let length = vmap.sections.length;
 	let idx = vmap.map.get(index);
-	//console.log(vmap, length, idx);
+
 	if (isAdd) {
 		idx = normalizeIndex(idx + 1, 0, length-1);
 	} else if (isSub) {
@@ -90,11 +130,13 @@ export function getFormatSection(format, value, index) {
 	let { vmap, fmap } = findSectionIndex(format, value);
 	let idx = vmap.map.get(index);
 	let fcur = fmap.sections[idx];
+
 	return format.substring(fcur.start, fcur.end);
 }
 
 export function findSectionIndex(format, value) {
 	let fmap = createSectionMap(format);
 	let vmap = createSectionMap(value);
+
 	return { fmap, vmap };
 }
